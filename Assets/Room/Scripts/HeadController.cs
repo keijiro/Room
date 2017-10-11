d4a5065fa26cf2195eefe39ec3afbd10b9d2a6a2
Keijiro Takahashi <keijiro@gmail.com>
2017-10-11 17:09:38 +0900

    Head components

---
+++ Assets/Room/Scripts/HeadController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.Timeline;
+using Klak.Math;
+
+namespace Room
+{
+    [ExecuteInEditMode]
+    public class HeadController : MonoBehaviour, ITimeControl
+    {
+        #region Editable attributes
+
+        [SerializeField] Vector3 _positionNoise;
+        [SerializeField] Vector3 _rotationNoise;
+        [SerializeField] float _noiseSpeed;
+        [SerializeField] float _gradientSpeed;
+        [SerializeField] float _accent;
+        [SerializeField] int _randomSeed;
+
+        #endregion
+
+        #region MonoBehaviour implementation
+
+        MaterialPropertyBlock _shaderSheet;
+        bool _underTimeControl;
+        float _time;
+
+        void Update()
+        {
+            if (_shaderSheet == null)
+                _shaderSheet = new MaterialPropertyBlock();
+
+            var hash = new XXHash(_randomSeed);
+            var time = _time * _noiseSpeed;
+            const int octaves = 2;
+
+            var pos = new Vector3(
+                Perlin.Fbm(hash.Range(-1e3f, 1e3f, 0) + time, octaves),
+                Perlin.Fbm(hash.Range(-1e3f, 1e3f, 1) + time, octaves),
+                Perlin.Fbm(hash.Range(-1e3f, 1e3f, 2) + time, octaves)
+            );
+
+            var rot = new Vector3(
+                Perlin.Fbm(hash.Range(-1e3f, 1e3f, 3) + time, octaves),
+                Perlin.Fbm(hash.Range(-1e3f, 1e3f, 4) + time, octaves),
+                Perlin.Fbm(hash.Range(-1e3f, 1e3f, 5) + time, octaves)
+            );
+
+            var trs = Matrix4x4.TRS(
+                Vector3.Scale(pos, _positionNoise),
+                Quaternion.Euler(Vector3.Scale(rot, _rotationNoise)),
+                Vector3.one
+            );
+
+            var offs = _gradientSpeed * _time + _randomSeed;
+
+            _shaderSheet.SetMatrix("_ExtraTransform", trs);
+            _shaderSheet.SetFloat("_GradOffs", offs);
+            GetComponent<MeshRenderer>().SetPropertyBlock(_shaderSheet);
+
+            if (!_underTimeControl)
+            {
+                if (Application.isPlaying)
+                    _time += Time.deltaTime;
+                else
+                    _time = 0;
+            }
+        }
+
+        #endregion
+
+        #region ITimeControl functions
+
+        public void OnControlTimeStart()
+        {
+            _underTimeControl = true;
+        }
+
+        public void OnControlTimeStop()
+        {
+            _underTimeControl = false;
+        }
+
+        public void SetTime(double time)
+        {
+            _time = (float)time;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Room/Shaders/Head.shader
@@ -0,0 +1,64 @@
+﻿Shader "Room/Head"
+{
+    Properties
+    {
+        _MainTex("Face Texture", 2D) = "gray" {}
+        [HDR] _Color("Color", Color) = (1, 1, 1, 1)
+        _GradFreq("Gradient Frequency", Float) = 4
+        _GradOffs("Gradient Offset", Float) = 0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        CGPROGRAM
+
+        #pragma surface Surface Standard vertex:Vertex addshadow nolightmap
+        #pragma target 3.0
+
+        #include "UnityCG.cginc"
+        #include "SimplexNoise3D.hlsl"
+
+        // Hue to RGB convertion
+        half3 HueToRGB(half h)
+        {
+            h = frac(h);
+            half r = abs(h * 6 - 3) - 1;
+            half g = 2 - abs(h * 6 - 2);
+            half b = 2 - abs(h * 6 - 4);
+            half3 rgb = saturate(half3(r, g, b));
+            return GammaToLinearSpace(rgb);
+        }
+
+        struct Input
+        {
+            float2 uv_MainTex;
+            float3 localCoord;
+        };
+
+        sampler2D _MainTex;
+        half4 _Color;
+        half _GradFreq;
+        float _GradOffs;
+
+        float4x4 _ExtraTransform;
+
+        void Vertex(inout appdata_full input, out Input data)
+        {
+            UNITY_INITIALIZE_OUTPUT(Input, data);
+            data.localCoord = input.vertex.xyz;
+            input.vertex = mul(_ExtraTransform, input.vertex);
+        }
+
+        void Surface(Input IN, inout SurfaceOutputStandard o)
+        {
+            float3 tex = tex2D(_MainTex, IN.uv_MainTex).rgb;
+            float hue = snoise((IN.localCoord.xyz + _GradOffs) * _GradFreq);
+            o.Albedo = HueToRGB(hue) * _Color.rgb * (1 - _Color.a);
+            o.Emission = tex * _Color.rgb * _Color.a;
+        }
+
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
